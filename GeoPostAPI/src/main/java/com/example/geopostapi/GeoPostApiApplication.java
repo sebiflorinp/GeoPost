f1330ff7f4576d5a2db66ed0fe079030ccf53eec
sebiflorinp <sebiflorinp@gmail.com>
2025-02-26 21:45:47 +0200

    ADD API

---
+++ GeoPostAPI/src/main/java/com/example/geopostapi/GeoPostApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.geopostapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoPostApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoPostApiApplication.class, args);
+    }
+
+}
